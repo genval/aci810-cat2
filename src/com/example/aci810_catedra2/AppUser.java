51ee8d4374fe80c0a137c34696163a905783f0d2
genval <gisse.encalada@gmail.com>
2013-11-17 17:26:47 -0300

    modif. activities and layout

--- src/com/example/aci810_catedra2/AppUser.java
+++ src/com/example/aci810_catedra2/AppUser.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class AppUser extends Activity {
 
@@ -23,10 +26,33 @@ public class AppUser extends Activity {
 		Boolean sexM = shadPref.getBoolean(MainActivity.MASCULINO,false);
 		Boolean sexO = shadPref.getBoolean(MainActivity.OTRO,false);	
 		
+		TextView user1 = (TextView)findViewById(R.id.emailField);
+		user1.setText(user);
+		TextView email1 = (TextView)findViewById(R.id.editText1);
+		email1.setText(email);
+		TextView edad1 = (TextView)findViewById(R.id.edad);
+		edad1.setText(edad);
+		EditText pass1 = (EditText)findViewById(R.id.passwordView);
+		pass1.setText(pass);
+		EditText pais1 = (EditText)findViewById(R.id.pais);
+		pais1.setText(pais);
+		RadioButton sexF1 = (RadioButton)findViewById(R.id.radio0);
+		sexF1.setChecked(sexF);
+		RadioButton sexM1 = (RadioButton)findViewById(R.id.radio1);
+		sexM1.setChecked(sexM);
+		RadioButton sexO1 = (RadioButton)findViewById(R.id.radio2);
+		sexO1.setChecked(sexO);
 	
+		setupActionBar();
 		
 	}
 
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
